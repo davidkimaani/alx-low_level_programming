5b2dab55480ef756642dce88b058d3eeea34a239
davidkimaani <davidkimani467@gmail.com>
2023-03-16 07:20:04 -0700

    6 size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- *
- * main - program that prints the size of various types on the computer it is compiled and run on
+ * main - main function
  *
  * Return: 0 when successful
  *
