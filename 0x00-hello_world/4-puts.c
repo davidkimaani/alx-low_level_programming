462e87557400642303f9c0d12f6b209f716aae20
davidkimaani <davidkimani467@gmail.com>
2023-03-16 05:41:29 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
+
 int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
-	return;
+	return (0);
 }
